b11d2027e99a39213593916d919e83cf8d408b51
Dustin <dustin.tamalun@gmail.com>
2024-07-29 14:37:10 +0200

    Fix unit tests

--- test/unit/core/tasks/widgets/task_box_test.dart
+++ test/unit/core/tasks/widgets/task_box_test.dart
@@ -1,7 +1,11 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:you_do/src/core/tasks/blocs/tasks_cubit.dart';
+import 'package:you_do/src/core/tasks/models/task.dart';
 import 'package:you_do/src/core/tasks/widgets/task_box.dart';
 
+import '../../../../mocks.dart';
 import '../../../../utils/golden_utils.dart';
 import '../../../../utils/test_util.dart';
 
@@ -10,17 +14,40 @@ void main() {
     final builder = getGoldenColumnBuilder()
       ..addScenario(
         "checked",
-        const TaskBox(
-          taskId: "1",
+        TaskBox(
+          Task(
+            id: "1",
+            description: "test description",
+            isCompleted: true,
+            dueDate: DateTime(2024),
+          ),
         ),
       )
       ..addScenario(
         "unchecked",
-        const TaskBox(
-          taskId: "2",
+        TaskBox(
+          Task(
+            id: "1",
+            description: "test description",
+            isCompleted: false,
+            dueDate: DateTime(2024),
+          ),
         ),
       );
-    await tester.pumpWidgetBuilder(builder.build(), wrapper: wrapWidget);
+    await tester.pumpWidgetBuilder(
+      builder.build(),
+      wrapper: (child) => wrapWidget(
+        child,
+        blocProviders: [
+          BlocProvider<TasksCubit>(
+            create: (context) => TasksCubit(
+              MockGetTasks(),
+              MockSaveTasks(),
+            ),
+          )
+        ],
+      ),
+    );
     await screenMatchesGolden(tester, "task_box");
   });
 }
